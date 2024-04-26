5d963f017e009e448cbf266149c607c3f3e06507
boluwatife adediwura <adediwuraboluwatife1@gmail.com>
2024-04-26 19:34:01 +0100

    Added changes to the index.css added some styling to the app

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def index():
 def diagnostic():
     return render_template("pages/diagnostic.html")
 
+@app.route("/symptom_result")
+def symptomresults():
+    return render_template("pages/result.html")
+
 if __name__ == "_main_":
 	app.run(host="0.0.0.0")
